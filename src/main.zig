4801aa87372336bffe1f34f14141781ff71b878c
Guzman Zugnoni <gooseiman@protonmail.com>
2024-12-06 08:56:18 -0300

    day5 part1

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,12 @@
 const std = @import("std");
-const day4 = @import("day4.zig");
+const day = @import("day5.zig");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const alloc = gpa.allocator();
 
-    const result = day4.run(alloc);
+    const result = day.run(alloc);
     std.debug.print("Part 1 sample: {any}\n", .{result[0]});
     std.debug.print("Part 1 puzzle: {any}\n", .{result[1]});
     std.debug.print("Part 2 sample: {any}\n", .{result[2]});
